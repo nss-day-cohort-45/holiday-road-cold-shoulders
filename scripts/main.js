d9663c393969dbd001d3a2fc79cd692270129f58
Gregory Thompson <mrgregorythompsonii@gmail.com>
2020-12-15 14:24:35 -0600

    cleared main.js

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-import {parkSelect} from "./parks/ParkSelect.js"
-import "./parks/ParkCard.js"
-import {AttractionSelect} from "./attractions/AttractionSelect.js"
-
-
-parkSelect()
-AttractionSelect()
