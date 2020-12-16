b94d470ad129a65a8167ad541d9967d4c62b8d1a
Ryan Youngblood <rryb77@gmail.com>
2020-12-16 15:59:06 -0600

    dialog generator is setup and working, waiting on customEvents from team

---
+++ scripts/Dialog/dialogShow.js
@@ -0,0 +1,35 @@
+const eventHub = document.querySelector('.container')
+const contentTarget = document.querySelector('.dialog')
+
+
+eventHub.addEventListener("theirEventName", e => {
+
+    // Grab the zipcode from the customEvent object and split it since more than one is passed
+    let title = e.detail.title
+    let body = e.detail.body
+    
+    render(title, body)
+});
+
+export const render = (title, body) => {
+    contentTarget.innerHTML = `
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">${title}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+        <div class="modal-body">
+        ${body} 
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+    `
+    
+    document.getElementById("btnModal").click()
+}
--- scripts/main.js
+++ scripts/main.js
@@ -7,7 +7,12 @@ import "./attractions/AttractionFilter.js"
 import "./weather/weatherList.js";
 import "./eateries/eateryList.js";
 import "./eateries/eateryFilter.js"
+import { render } from "./Dialog/dialogShow.js";
 
 parkSelect();
 AttractionSelect();
 eaterySelect();
+
+let title = 'Here is my title'
+let body ='and here is my body'
+render(title, body)
