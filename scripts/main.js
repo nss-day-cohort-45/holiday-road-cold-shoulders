2e76eb6e8a910a74a2b0421b072b9bcd913329b6
cslunetta <cslunetta@outlook.com>
2020-12-15 22:05:19 -0600

    spaced and centered dropdown buttons and updated main to show Items on DOM

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
 import "./parks/ParkCard.js";
 import { parkSelect } from "./parks/ParkSelect.js";
 import { eateryList } from "./eateries/eateryList.js";
+import { AttractionSelect } from "./attractions/AttractionSelect.js";
+import { weatherList } from "./weather/weatherList.js";
 
 parkSelect();
+AttractionSelect();
 eateryList();
+weatherList();
